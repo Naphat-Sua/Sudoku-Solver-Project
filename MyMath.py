1f1368262ac3fe2f9af5ed3f9a7fe057297c060d
Naphat-Sua <naphat.sua@dome.tu.ac.th>
2024-10-10 17:54:43 +0700

    Create MyMath.py

---
+++ MyMath.py
@@ -0,0 +1,13 @@
+public class MyMath {
+    public static int calculate(int x, int y) {
+        return x + y;
+    }
+
+    public static int floor(int i) {
+        return 1;
+    }
+    
+    public static int ceil(int j) {
+        return 9;
+    }
+}
